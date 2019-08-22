956e1023eb85cb5f45443e51fc3d8606ad07c3f0
luckyboy0057 <iamirfanwebdeveloper@gmail.com>
2019-08-22 16:29:56 +0530

    question code

--- resources/views/auth/social/buttons.blade.php
+++ resources/views/auth/social/buttons.blade.php
@@ -1,7 +1,7 @@
 @if ($socialProviders)
     <?php $colSize = 12 / count($socialProviders); ?>
 
-    <div class="row pb-3 pt-2">
+    <!-- <div class="row pb-3 pt-2">
         @if (in_array('facebook', $socialProviders))
             <div class="col-{{ $colSize }} d-flex align-items-center justify-content-center">
                 <a href="{{ url('auth/facebook/login') }}" class="btn-facebook">
@@ -25,6 +25,6 @@
                 </a>
             </div>
         @endif
-    </div>
+    </div> -->
 
 @endif
